c649f9dad9dc4998bfce5e20d77496df5a0b9441
srianumakonda <sri.anumakonda06@gmail.com>
2021-04-02 13:14:29 -0400

    Updated EKF project files

--- CarND-Extended-Kalman-Filter-Project/src/kalman_filter.cpp
+++ CarND-Extended-Kalman-Filter-Project/src/kalman_filter.cpp
@@ -3,14 +3,11 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-/* 
- * Please note that the Eigen library does not initialize 
- *   VectorXd or MatrixXd objects with zeros upon creation.
- */
-
 KalmanFilter::KalmanFilter() {}
 
 KalmanFilter::~KalmanFilter() {}
+
+Tools KalmanFilter::tools_res = Tools();
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
                         MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
@@ -20,22 +17,63 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
   H_ = H_in;
   R_ = R_in;
   Q_ = Q_in;
+
+// remember that we're assuming that there is 0 noise; therefore, u does not need to be specified
 }
 
 void KalmanFilter::Predict() {
-  /**
-   * TODO: predict the state
-   */
+  
+  x_ = F_*x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_*P_*Ft+Q_;
+
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Kalman Filter equations
-   */
+
+  MatrixXd I_ = MatrixXd::Identity(x_.size(),x_.size());
+  
+  VectorXd y_ = z - H_ * x_;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S_ = H_ * P_ * Ht + R_;
+  MatrixXd Si = S_.inverse();
+  MatrixXd K_ =  P_ * Ht * Si;
+
+  x_ = x_ + (K_ * y_);
+  P_ = (I_ - K_ * H_) * P_;
+
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Extended Kalman Filter equations
-   */
+  
+  MatrixXd I_ = MatrixXd::Identity(x_.size(),x_.size());
+  float x = ekf_.x_(0);
+  float y = ekf_.x_(1);
+  float vx = ekf_.x_(2);
+  float vy = ekf_.x_(3);
+
+  float rho = sqrt(x*x+y*y);
+  float thetha = atan2(y/x);
+  float rho_dot = (x*vx+y*vy)/rho;
+
+  VectorXd z_pred = VectorXd(3);
+  z_pred << rho,thetha,rho_dot;
+  
+  VectorXd y_ = z - z_pred;
+
+  // while (y_[1] < -M_PI){
+  //   y_[1] += 2 * M_PI;
+  // }
+  // while (y_[1] > M_PI){
+  //   y_[1] -= 2 * M_PI;
+  // }
+
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S_ = H_ * P_ * Ht + R_;
+  MatrixXd Si = S_.inverse();
+  MatrixXd K_ =  P_ * Ht * Si;
+
+  x_ = x_ + (K_ * y_);
+  P_ = (I_ - K_ * H_) * P_;
+
 }
