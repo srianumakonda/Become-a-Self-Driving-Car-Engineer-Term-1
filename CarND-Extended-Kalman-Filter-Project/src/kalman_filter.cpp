7f18d53b40caf33552a85f5c1f7cf046a5cd1afa
srianumakonda <sri.anumakonda06@gmail.com>
2021-04-02 17:27:29 -0400

    updated files

--- CarND-Extended-Kalman-Filter-Project/src/kalman_filter.cpp
+++ CarND-Extended-Kalman-Filter-Project/src/kalman_filter.cpp
@@ -57,10 +57,10 @@ void KalmanFilter::Update(const VectorXd &z) {
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
 
-  float px = x_(0);
-  float py = x_(1);
-  float vx = x_(2);
-  float vy = x_(3);
+  double px = x_(0);
+  double py = x_(1);
+  double vx = x_(2);
+  double vy = x_(3);
 
   float rho = sqrt(px*px+py*py);
   float phi = atan2(py,px);
